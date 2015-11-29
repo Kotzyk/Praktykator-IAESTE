1c8be30372a859d1f245549e871ed6920672ce72
Mateusz <matico575@gmail.com>
2015-11-29 23:40:02 +0100

    Poprawki nazwy paczki c.d.

---
+++ app/src/main/java/pl/edu/agh/iaeste/praktykator/Menu.java
@@ -0,0 +1,34 @@
+package pl.edu.agh.iaeste.praktykator;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by Mateusz on 2015-11-29.
+ */
+
+public class Menu extends ListActivity{
+ String pliki[] = {"MainActivity","dwa","trzy","cztery","piec"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<>(Menu.this, android.R.layout.simple_list_item_1, pliki));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String cheese = pliki[position];
+        try {
+            Class klasa = Class.forName("pl.edu.agh.iaeste.praktykator."+cheese);
+            Intent intent1 = new Intent(Menu.this, klasa);
+            startActivity(intent1);
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/pl/edu/agh/iaeste/praktykator/Splash.java
@@ -0,0 +1,37 @@
+package pl.edu.agh.iaeste.praktykator;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import pl.edu.agh.iaeste.praktykator.R;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        Thread timer = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(4000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent startMain = new Intent("pl.edu.agh.iaeste.praktykator.MENU");
+                    startActivity(startMain);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
