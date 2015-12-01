fba940d5a7b4622a86d71e42d766b9623ee4ddd0
Mateusz <matico575@gmail.com>
2015-12-01 18:43:25 +0100

    Poprawki do ListView
    Wciąż nie działa, bo zwraca pusty array messages, ale  "ja to naprawię"

--- app/src/main/java/pl/edu/agh/iaeste/praktykator/Menu.java
+++ app/src/main/java/pl/edu/agh/iaeste/praktykator/Menu.java
@@ -8,10 +8,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 import java.util.Properties;
 
 import javax.mail.Address;
@@ -21,125 +18,113 @@ import javax.mail.Store;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
-/**
- * Created by Mateusz on 2015-11-29.
- */
+public class Menu extends ListActivity {
+    String protocol = "imaps";
+    String host = "imaps.gmail.com";
+    String port = "993";
+    String usr = "matico575@gmail.com";
+    String pwd = "4PanceriPies";
+    Message[] maile;
 
-public class Menu extends ListActivity{
-    String protocol = "pop3";
-    String host = "pop.gmail.com";
-    String port = "995";
-    String usr = String.valueOf(R.string.userName);
-    String pwd = String.valueOf(R.string.password);
-    List<Message> maile = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //zwinieta jest cala dawna klasa EmailReceiver
+        Properties properties = getServerProperties(protocol, host, port);
+        //Setting IMAP session
+        Session imapSession = Session.getInstance(properties);
 
+        try {
+            Store store = imapSession.getStore("imaps");
+            //Connect to server by sending username and password.
+            store.connect("imap.gmail.com", usr, pwd);
+            //Get all mails in Inbox Forlder
+            Folder inbox = store.getFolder("Inbox");
+            inbox.open(Folder.READ_ONLY);
+            //Return result to array of message
+            Message[] messages = inbox.getMessages();
 
-            Properties properties = getServerProperties(protocol, host, port);
-            Session session = Session.getDefaultInstance(properties);
+            for (int i = 0; i < messages.length; i++) {
+                Message msg = messages[i];
+                Address[] fromAddress = msg.getFrom();
+                String from = fromAddress[0].toString();
+                String subject = msg.getSubject();
+                String toList = parseAddresses(msg
+                        .getRecipients(javax.mail.Message.RecipientType.TO));
+                String ccList = parseAddresses(msg
+                        .getRecipients(Message.RecipientType.CC));
+                String sentDate = msg.getSentDate().toString();
 
-            try {
-                // connects to the message store
-                Store store = session.getStore(protocol);
-                store.connect(usr, pwd);
+                String contentType = msg.getContentType();
+                String messageContent = "";
 
-                // opens the inbox folder
-                Folder folderInbox = store.getFolder("INBOX");
-                folderInbox.open(Folder.READ_ONLY);
-
-                // fetches new messages from server
-                Message[] messages = folderInbox.getMessages();
-
-                for (int i = 0; i < messages.length; i++) {
-                    Message msg = messages[i];
-                    Address[] fromAddress = msg.getFrom();
-                    String from = fromAddress[0].toString();
-                    String subject = msg.getSubject();
-                    String toList = parseAddresses(msg
-                            .getRecipients(javax.mail.Message.RecipientType.TO));
-                    String ccList = parseAddresses(msg
-                            .getRecipients(Message.RecipientType.CC));
-                    String sentDate = msg.getSentDate().toString();
-
-                    String contentType = msg.getContentType();
-                    String messageContent = "";
-
-                    if (contentType.contains("text/plain")
-                            || contentType.contains("text/html")) {
-                        try {
-                            Object content = msg.getContent();
-                            if (content != null) {
-                                messageContent = content.toString();
-                            }
-                        } catch (Exception ex) {
-                            messageContent = "[Error downloading content]";
-                            ex.printStackTrace();
+                if (contentType.contains("text/plain")
+                        || contentType.contains("text/html")) {
+                    try {
+                        Object content = msg.getContent();
+                        if (content != null) {
+                            messageContent = content.toString();
                         }
+                    } catch (Exception ex) {
+                        messageContent = "[Error downloading content]";
+                        ex.printStackTrace();
                     }
                 }
-                maile = Arrays.asList(messages);
-
-                // disconnect
-                folderInbox.close(false);
-                store.close();
-            } catch (NoSuchProviderException ex) {
-                System.out.println("No provider for protocol: " + protocol);
-                ex.printStackTrace();
-            } catch (MessagingException ex) {
-                System.out.println("Could not connect to the message store");
-                ex.printStackTrace();
             }
-        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, maile));
+            maile = messages.clone();
+
+            // disconnect
+            inbox.close(false);
+            store.close();
+        } catch (NoSuchProviderException ex) {
+            System.out.println("No provider for protocol: " + protocol);
+            ex.printStackTrace();
+        } catch (MessagingException ex) {
+            System.out.println("Could not connect to the message store");
+            ex.printStackTrace();
         }
+            setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, maile));
+    }
 
-        private String parseAddresses(Address[] address) {
-            String listAddress = "";
-
-            if (address != null) {
-                for (int i = 0; i < address.length; i++) {
-                    listAddress += address[i].toString() + ", ";
-                }
-            }
-            if (listAddress.length() > 1) {
-                listAddress = listAddress.substring(0, listAddress.length() - 2);
-            }
-
-            return listAddress;
-        }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
 
-            Intent intent1 = new Intent();
-            startActivity(intent1);
-
     }
 
-
-    private Properties getServerProperties(String protocol, String host,
-                                           String port) {
+    private Properties getServerProperties(String protocol, String host, String port) {
         Properties properties = new Properties();
 
         // server setting
-        properties.put(String.format("mail.%s.host", protocol), host);
-        properties.put(String.format("mail.%s.port", protocol), port);
+        properties.setProperty("mail.store.protocol", protocol);
+        properties.setProperty("mail.imaps.host", host);
+        properties.setProperty("mail.imaps.port", port);
 
         // SSL setting
-        properties.setProperty(
-                String.format("mail.%s.socketFactory.class", protocol),
-                "javax.net.ssl.SSLSocketFactory");
-        properties.setProperty(
-                String.format("mail.%s.socketFactory.fallback", protocol),
-                "false");
-        properties.setProperty(
-                String.format("mail.%s.socketFactory.port", protocol),
-                String.valueOf(port));
+        properties.setProperty("mail.imaps.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        properties.setProperty("mail.imaps.socketFactory.fallback", "false");
 
         return properties;
+
+    }
+
+    private String parseAddresses(Address[] address) {
+        String listAddress = "";
+
+        if (address != null) {
+            for (int i = 0; i < address.length; i++) {
+                listAddress += address[i].toString() + ", ";
+            }
+        }
+        if (listAddress.length() > 1) {
+            listAddress = listAddress.substring(0, listAddress.length() - 2);
+        }
+
+        return listAddress;
     }
 }
 
