4bd5e9a8223ad60e702670e762b4a0afeb47f756
Mateusz <matico575@gmail.com>
2015-12-03 20:51:31 +0100

    DZIAŁA POBIERANIE MAILI
    Tylko teraz nie umiem w nagłówki tych maili ani tym bardziej załączniki

--- app/src/main/java/pl/edu/agh/iaeste/praktykator/Menu.java
+++ app/src/main/java/pl/edu/agh/iaeste/praktykator/Menu.java
@@ -1,104 +1,54 @@
 package pl.edu.agh.iaeste.praktykator;
 
 import android.app.ListActivity;
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 
 import java.util.Properties;
 
 import javax.mail.Address;
 import javax.mail.Folder;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Store;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 public class Menu extends ListActivity {
     String protocol = "imaps";
     String host = "imaps.gmail.com";
     String port = "993";
-    String usr = "iaesteaghpraktyki";
+    String usr = "iaesteaghpraktyki@gmail.com";
     String pwd = "projektandroid";
     Message[] maile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //zwinieta jest cala dawna klasa EmailReceiver
+
         Properties properties = getServerProperties(protocol, host, port);
-        //Setting IMAP session
-        Session imapSession = Session.getInstance(properties);
 
-        try {
-            Store store = imapSession.getStore("imaps");
-            //Connect to server by sending username and password.
-            store.connect("imap.gmail.com", usr, pwd);
-            //Get all mails in Inbox Forlder
-            Folder inbox = store.getFolder("Inbox");
-            inbox.open(Folder.READ_ONLY);
-            //Return result to array of message
-            Message[] messages = inbox.getMessages();
-
-            for (int i = 0; i < messages.length; i++) {
-                Message msg = messages[i];
-                Address[] fromAddress = msg.getFrom();
-                String from = fromAddress[0].toString();
-                String subject = msg.getSubject();
-                String toList = parseAddresses(msg
-                        .getRecipients(javax.mail.Message.RecipientType.TO));
-                String ccList = parseAddresses(msg
-                        .getRecipients(Message.RecipientType.CC));
-                String sentDate = msg.getSentDate().toString();
-
-                String contentType = msg.getContentType();
-                String messageContent = "";
-
-                if (contentType.contains("text/plain")
-                        || contentType.contains("text/html")) {
-                    try {
-                        Object content = msg.getContent();
-                        if (content != null) {
-                            messageContent = content.toString();
-                        }
-                    } catch (Exception ex) {
-                        messageContent = "[Error downloading content]";
-                        ex.printStackTrace();
-                    }
-                }
-            }
-            maile = messages.clone();
-
-            // disconnect
-            inbox.close(false);
-            store.close();
-        } catch (NoSuchProviderException ex) {
-            System.out.println("No provider for protocol: " + protocol);
-            ex.printStackTrace();
-        } catch (MessagingException ex) {
-            System.out.println("Could not connect to the message store");
-            ex.printStackTrace();
-        }
-            //Wiem, ze to nie dziala, ale to tylko tak testowo
-            // setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, maile));
+        new EmailDownloader(this).execute(properties);
     }
 
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-
     }
 
     private Properties getServerProperties(String protocol, String host, String port) {
         Properties properties = new Properties();
+
+        // user props
+        properties.setProperty("username", usr);
+        properties.setProperty("password",pwd);
 
         // server setting
         properties.setProperty("mail.store.protocol", protocol);
@@ -113,19 +63,99 @@ public class Menu extends ListActivity {
 
     }
 
-    private String parseAddresses(Address[] address) {
-        String listAddress = "";
+    private class EmailDownloader extends AsyncTask<Properties, Void, Message[]> {
 
-        if (address != null) {
-            for (int i = 0; i < address.length; i++) {
-                listAddress += address[i].toString() + ", ";
+        public Menu activity;
+
+        public EmailDownloader(Menu menu) {
+            this.activity = menu;
+        }
+
+        @Override
+        protected Message[] doInBackground(Properties... arg0) {
+
+            final Properties props = arg0[0];
+
+            Session imapSession = Session.getInstance(props, new javax.mail.Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(props.getProperty("username"), props.getProperty("password"));
+                }
+            });
+
+            try {
+                Store store = imapSession.getStore("imaps");
+                //Connect to server by sending username and password.
+                store.connect("imap.gmail.com", props.getProperty("username"), props.getProperty("password"));
+                //Get all mails in Inbox Forlder
+                Folder inbox = store.getFolder("Inbox");
+                inbox.open(Folder.READ_ONLY);
+                //Return result to array of message
+                Message[] messages = inbox.getMessages();
+
+                for (int i = 0; i < messages.length; i++) {
+                    Message msg = messages[i];
+                    Address[] fromAddress = msg.getFrom();
+                    String from = fromAddress[0].toString();
+                    String subject = msg.getSubject();
+                    String toList = parseAddresses(msg
+                            .getRecipients(javax.mail.Message.RecipientType.TO));
+                    String ccList = parseAddresses(msg
+                            .getRecipients(Message.RecipientType.CC));
+                    String sentDate = msg.getSentDate().toString();
+
+                    String contentType = msg.getContentType();
+                    String messageContent = "";
+
+                    if (contentType.contains("text/plain")
+                            || contentType.contains("text/html")) {
+                        try {
+                            Object content = msg.getContent();
+                            if (content != null) {
+                                messageContent = content.toString();
+                            }
+                        } catch (Exception ex) {
+                            messageContent = "[Error downloading content]";
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+                maile = messages.clone();
+
+                // disconnect
+                inbox.close(false);
+                store.close();
+
+            } catch (NoSuchProviderException ex) {
+                Log.e("EmailDownloader", "No provider for protocol: " + protocol);
+                ex.printStackTrace();
+            } catch (MessagingException ex) {
+                Log.e("EmailDownloader", "Could not connect to the message store");
+                ex.printStackTrace();
             }
-        }
-        if (listAddress.length() > 1) {
-            listAddress = listAddress.substring(0, listAddress.length() - 2);
+
+            return maile;
         }
 
-        return listAddress;
+        @Override
+        protected void onPostExecute(Message[] results) {
+            activity.setListAdapter(new ArrayAdapter<>(activity, android.R.layout.simple_list_item_1, results));
+        }
+
+        private String parseAddresses(Address[] address) {
+            String listAddress = "";
+
+            if (address != null) {
+                for (int i = 0; i < address.length; i++) {
+                    listAddress += address[i].toString() + ", ";
+                }
+            }
+            if (listAddress.length() > 1) {
+                listAddress = listAddress.substring(0, listAddress.length() - 2);
+            }
+
+            return listAddress;
+        }
+
     }
 }
 
